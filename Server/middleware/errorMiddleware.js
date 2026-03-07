251032165b0e03fc77b198980b7ddc255dbbfe5f
nitikamaan <nitikamaan15209@gmail.com>
2026-03-08 03:08:19 +0530

    Delete Server directory

--- Server/middleware/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-
-    res.status(500).json({
-        message: err.message
-    });
-
-};
--- Server/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderSchema = new mongoose.Schema({
-    userId: mongoose.Schema.Types.ObjectId,
-    products: Array,
-    total: Number
-}, { timestamps: true });
-
-module.exports = mongoose.model("Order", orderSchema);
